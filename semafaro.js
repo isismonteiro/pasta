5bcc735669f1f61e46277872d7184996ccfbf3fe
isismonteiro <isislorenamonteiro@gmail.com>
2026-09-20 22:23:18 -0300

    first commit

---
+++ semafaro.js
@@ -0,0 +1,30 @@
+
+ function somar() {
+ let n1 = Number(document.getElementById("numero1").value);
+ let n2 = Number(document.getElementById("numero2").value);
+ let resultado = n1 + n2;
+ document.getElementById("resultado").innerHTML = resultado;
+ }
+
+ function subtrair() {
+ let n1 = Number(document.getElementById("numero1").value);
+ let n2 = Number(document.getElementById("numero2").value);
+ let resultado = n1 - n2;
+ document.getElementById("resultado").innerHTML = resultado;
+ }
+
+ function multiplicar() {
+ let n1 = Number(document.getElementById("numero1").value);
+ let n2 = Number(document.getElementById("numero2").value);
+ let resultado = n1 * n2;
+ document.getElementById("resultado").innerHTML = resultado;
+ }
+ 
+ function dividir() {
+ let n1 = Number(document.getElementById("numero1").value);
+ let n2 = Number(document.getElementById("numero2").value);
+ let resultado = n1 / n2;
+ document.getElementById("resultado").innerHTML = resultado;
+ }
+
+ 
